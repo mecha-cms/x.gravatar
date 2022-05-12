ebc64ade5cefc8e742f4a79edf967b089ccdf314
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-05-12 23:42:57 +0700

    Update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 function gravatar($avatar, array $lot = []) {
     $size = \array_shift($lot) ?: 80;
-    return \sprintf('https://www.gravatar.com/avatar/%1$s?s=%2$d&d=mp', \md5($this['email'] ?? ""), $size);
+    return 'https://www.gravatar.com/avatar/' . \md5($this['email'] ?? "") . '?s=' . $size . '&d=mp';
 }
 
-\Hook::set('page.avatar', __NAMESPACE__ . "\\gravatar", 2);
+\Hook::set('page.avatar', __NAMESPACE__ . "\\gravatar", 1);
